4859825b4cce120bfaa1cbc184181973f3611de3
Chris Holdgraf <choldgraf@berkeley.edu>
2020-02-22 22:17:03 -0800

    black

--- setup.py
+++ setup.py
@@ -3,28 +3,24 @@ import os
 from setuptools import setup, find_packages
 from sphinx_togglebutton import __version__
 
-with open('./README.rst', 'r') as ff:
+with open("./README.rst", "r") as ff:
     readme_text = ff.read()
 
 setup(
-    name='sphinx-togglebutton',
+    name="sphinx-togglebutton",
     version=__version__,
     description="Add a toggle button to items on a page.",
     long_description=readme_text,
-    long_description_content_type='text/x-rst',
-    author='Chris Holdgraf',
-    author_email='choldgraf@berkeley.edu',
+    long_description_content_type="text/x-rst",
+    author="Chris Holdgraf",
+    author_email="choldgraf@berkeley.edu",
     url="https://github.com/choldgraf/sphinx-togglebutton",
-    license='MIT License',
+    license="MIT License",
     packages=find_packages(),
-    package_data={'sphinx_togglebutton': [
-        '_static/togglebutton.css',
-        '_static/togglebutton.js'
-        ]
+    package_data={
+        "sphinx_togglebutton": ["_static/togglebutton.css", "_static/togglebutton.js"]
     },
     install_requires=["setuptools", "wheel", "sphinx"],
-    extras_require={
-        "sphinx": ["sphinx"]
-    }
-    classifiers=["License :: OSI Approved :: MIT License"]
+    extras_require={"sphinx": ["sphinx"]},
+    classifiers=["License :: OSI Approved :: MIT License"],
 )
--- sphinx_togglebutton/__init__.py
+++ sphinx_togglebutton/__init__.py
@@ -5,32 +5,36 @@ __version__ = "0.0.2dev0"
 
 
 def st_static_path(app):
-    static_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '_static'))
+    static_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "_static"))
     app.config.html_static_path.append(static_path)
+
 
 # This function reads in a variable and inserts it into JavaScript
 def insert_custom_selection_config(app):
     # This is a configuration that you've specified for users in `conf.py`
-    selector = app.config['togglebutton_selector']
+    selector = app.config["togglebutton_selector"]
     js_text = "var togglebuttonSelector = '%s';" % selector
     app.add_js_file(None, body=js_text)
+
 
 # We connect this function to the step after the builder is initialized
 def setup(app):
     # Add our static path
-    app.connect('builder-inited', st_static_path)
+    app.connect("builder-inited", st_static_path)
 
     # Add relevant code to headers
-    app.add_stylesheet('togglebutton.css')
+    app.add_stylesheet("togglebutton.css")
 
     # Add the string we'll use to select items in the JS
     # Tell Sphinx about this configuration variable
-    app.add_config_value('togglebutton_selector', ".toggle", "html")
-    app.add_js_file('togglebutton.js')
+    app.add_config_value("togglebutton_selector", ".toggle", "html")
+    app.add_js_file("togglebutton.js")
 
     # Run the function after the builder is initialized
-    app.connect('builder-inited', insert_custom_selection_config)
+    app.connect("builder-inited", insert_custom_selection_config)
 
-    return {"version": __version__,
-            "parallel_read_safe": True,
-            "parallel_write_safe": True}
+    return {
+        "version": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }

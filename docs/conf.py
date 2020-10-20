165e05d44266c7c5022ccc293c36f05bf78338c6
Matthias Geier <Matthias.Geier@gmail.com>
2020-10-20 18:57:55 +0200

    📚 DOCS: Disable pygments_style setting (#25)

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ language = None
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
+#pygments_style = "sphinx"
 
 
 # -- Options for HTML output -------------------------------------------------

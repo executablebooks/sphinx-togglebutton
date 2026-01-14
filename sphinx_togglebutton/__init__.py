ec20337132f82111541874c155f7f9ffa7d1c9a6
Dennis den Ouden-van der Horst <49307154+douden@users.noreply.github.com>
2026-01-14 15:32:03 +0100

    Update release workflow and bump version to 0.4.4 (#73)
    Upgraded GitHub Actions versions and corrected build commands in tests.yml for improved reliability. Updated CHANGELOG and bumped package version to 0.4.4.

--- sphinx_togglebutton/__init__.py
+++ sphinx_togglebutton/__init__.py
@@ -9,7 +9,7 @@ from sphinx.locale import get_translation
 MESSAGE_CATALOG_NAME = "togglebutton"
 translate = get_translation(MESSAGE_CATALOG_NAME)
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 
 
 def st_static_path(app):
